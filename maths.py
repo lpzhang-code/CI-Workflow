b368fc99d9911e640662508e76003529c4950b0e
Peter Zhang <lpzhang@me.com>
2020-11-17 10:50:12 +1100

    Update config and logic

--- maths.py
+++ maths.py
@@ -1,6 +1,14 @@
-def square(a):
-    return a * a
-
+def add(a, b):
+    return a + b
 
 def subtract(a, b):
     return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide (a, b):
+    return a / b
+
+def square(a):
+    return a * a
--- test_maths.py
+++ test_maths.py
@@ -1,11 +1,27 @@
+from maths import *
 import pytest
 
-from maths import square, subtract
+
+def test_add():
+    assert add(1, 5) == 6
+    assert not add(2, 7) == 4
+
+
+def test_subtract():
+    assert subtract(6, 4) == 2
+    assert not subtract(5, 2) == 6
+
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+    assert not multiply(8, 7) == 50
+
+
+def test_divide():
+    assert divide(20, 2) == 10
+    assert not divide(9, 3) == 2
 
 
 def test_square():
     assert square(5) == 25
-
-
-def test_subtract():
-    assert subtract(100, 10) == 90
+    assert not square(3) == 10
